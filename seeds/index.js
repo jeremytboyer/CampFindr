9a94073e3017500fc551fc7035ab16f0db959e29
jeremtboyer <jeremy@ableengine.com>
2023-02-03 13:28:50 -0500

    initial commit

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const cities = require('./cities')
 const Campground = require('../models/campground');
 const {places, descriptors} = require('./seedHelpers')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
